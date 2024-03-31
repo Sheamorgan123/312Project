72b18874dddf589466a238ca1015f6936c6f29a3
Michelle Cortes <122902193+mika-ela011@users.noreply.github.com>
2024-03-31 17:00:46 -0600

    Create index.php
    basic shell of index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'header.php' ?>
+</head>
+<body>
+  <header>
+    <?php include 'navbar.php' ?>
+  </header>
+  <main>
+    <!--  Placeholder -->
+  </main>
+</body>
+<footer>
+  <?php include 'footer.php' ?>
+</footer>
+</html>
