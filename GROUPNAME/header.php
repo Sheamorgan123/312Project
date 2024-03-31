556ee97f4320945a55cb001e6c671b04d6477cb2
Michelle Cortes <122902193+mika-ela011@users.noreply.github.com>
2024-03-31 17:21:49 -0600

    Create header.php

---
+++ GROUPNAME/header.php
@@ -0,0 +1,6 @@
+  <meta charset="UTF-8">
+  <title>GROUPNAME | Homepage </title>
+  <meta name="description" content="This website is for the CS 312 Web Design course.">
+  <meta name="author" content="Michelle Cortes, Shea Spalding">
+  <meta name="keywords" content="CS312, colors">
+<!--   <link rel="stylesheet" type="text/css" href="east_index.css"> -->
