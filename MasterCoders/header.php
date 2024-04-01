cbeffaf1392940fa310c6b629b4f8e30d3cd410a
Michelle Cortes <122902193+mika-ela011@users.noreply.github.com>
2024-04-01 15:40:11 -0600

    Update header.php to MasterCoders

--- MasterCoders/header.php
+++ MasterCoders/header.php
@@ -1,5 +1,5 @@
   <meta charset="UTF-8">
-  <title>GROUPNAME | Homepage </title>
+  <title>MasterCoders | Homepage </title>
   <meta name="description" content="This website is for the CS 312 Web Design course.">
   <meta name="author" content="Arostegui,Janette, Michelle Cortes, Shea Spalding, Varonika Wilson">
   <meta name="keywords" content="CS312, colors">
