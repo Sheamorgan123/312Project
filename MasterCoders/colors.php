1567b875e96c4e32f56dd5a7662a429f623b5cd6
Varonika Wilson <wilsonvaronika@gmail.com>
2024-04-02 14:34:47 -0600

    Form finalized

--- MasterCoders/colors.php
+++ MasterCoders/colors.php
@@ -9,9 +9,9 @@
   </header>
   <main>
     <!-- 2 inputs, one for number of rows/columns, one for number of colors  -->
-    <form method="get" action="<?php>
+    <form method="get" action="<?php
       $n = $_GET["n"];
-      $num_colors = $_GET["color"];
+      $num_colors = $_GET["num_colors"];
 
       $n_warning = 'Table size must be a value between 1 and 26';
       $num_colors_warning = 'Color must be a value between 1 and 10';
@@ -21,23 +21,33 @@
       $num_colors_warning_display = false;
       $both_warning_display = false;
 
-      if (isset($_GET["n"]) && $n > 26 && $n < 1){
-        // Error
+      if (!isset($_GET["n"])){
+      } else if ($n > 26 || $n < 1){
         $n_warning_display = true;
-      } else if(isset($_GET["color"]) && $color > 10 && $color < 1){
+      }
+      
+      if(!isset($_GET["num_colors"])){
+      } else if($num_colors > 10 || $num_colors < 1){
         $num_colors_warning_display = true;
       }
 
       if($n_warning_display && $num_colors_warning_display){
         $both_warning_display = true;
       }
+
     ?>">
+
     <label for="n">Table Size(1-26)</label>
     <input type="text" name="n" id="n">
 
-    <label for="num_color">Color(1-10)</label>
-    <input type="num_color" name="num_color" id="num_color">
-    <span style="color:red;"><?php if($both_warning_display){echo $both_warning; }else if($n_warning_display){echo $n_warning; }else if($num_colors_warning_display){echo $color_warning; }?></span>
+    <label for="num_colors">Color(1-10)</label>
+    <input type="text" name="num_colors" id="num_colors">
+    <span style="color:red;"><?php 
+    if($both_warning_display){echo $both_warning; }
+    elseif($n_warning_display){echo $n_warning; }
+    elseif($num_colors_warning_display){echo $num_colors_warning; } else{echo '';}
+    ?>
+    </span>
 
     <input type="submit" value="generate tables">
     </form>
