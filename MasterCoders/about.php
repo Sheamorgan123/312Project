0b8c40d1afae3bba8093313117a515da19a1f326
janettearo <janettearostegui@gmail.com>
2024-04-02 22:09:47 -0600

    added the base about team member

--- MasterCoders/about.php
+++ MasterCoders/about.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Team Master Coders</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
+    <p>Janette Arostegui</p>
+    <p>Hi, my name is Janette and my major is Computer Science</p>
+    <p>Some of my favorite hobbies are to read and to play tennis</p>
 
+    <footer>
+        <p>Team member info <br>
+        <a href="mailto:janette@colostate.edu">janette@colostate.edu</a></p>
+        <a href="team_member1.php">Team member 1</a><br>
+        <a href="team_member2.php">Team member 2</a><br>
+        <a href="team_member3.php">Team member 3</a><br>
+    </footer>
+</body>
+</html>
