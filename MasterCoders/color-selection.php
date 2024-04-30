c2b0c20a65953c42b57b343ac03a59d7d449fafc
mika-ela011 <michelle.r.cortes@hotmail.com>
2024-04-30 17:45:31 -0600

    delete implementation

--- MasterCoders/color-selection.php
+++ MasterCoders/color-selection.php
@@ -10,10 +10,10 @@
     <?php include 'navbar.html' ?>
   </header>
   <main>
-    <?php
+    <?php 
     /*
-    code for the database connection,
-    everyone needs to change this to your own database params.
+    Code for the database connection,
+    Everyone needs to change this to your own database params.
     */
     $servername = "faure";
     $username = "";
@@ -27,16 +27,6 @@
     }
     echo "<script>console.log('Successfully connected to server.');</script>";
     ?>
-
-    <h2>Add/Edit/Delete Colors</h2>
-    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-      <h3>Add a Color:</h3>
-      Color Name: <input type="text" name="color_name" required><br>
-      Hex Value: <input type="text" name="hex_value" pattern="#[0-9a-fA-F]{6}" title="Enter a valid hex color value (e.g., #RRGGBB)" required><br>
-      <input type="submit" name="add_color" value="Add Color">
-      <h3>Delete a Color:</h3>
-      <h3>Edit a Color:</h3>
-    </form>
 
     <?php
       if (isset($_POST['add_color'])) {
@@ -64,12 +54,64 @@
           }
         }
       }
+
+      if (isset($_POST['delete_color'])) {
+        // To check there is at least 2 colors
+        $count_query = "SELECT COUNT(*) AS count FROM colors";
+        $count_result = $conn->query($count_query);
+        $row = $count_result->fetch_assoc();
+        $color_count = $row['count'];
+        
+        // Actually checks
+        if ($color_count > 2) {
+          $color = $_POST['colors_list'];
+          $delete_query = $conn->prepare("DELETE FROM colors WHERE id=?");
+          $delete_query->bind_param("i", $color);
+          $deletion_result = $delete_query->execute();
+
+          $colors_query = "SELECT id, Name FROM colors";
+          $colors_result = $conn->query($colors_query);
+
+          // If successfully deletes from database
+          if ($deletion_result === TRUE) {
+            echo "<p style='color:green;'>Color deleted successfully.</p>";
+          }
+          else {
+            echo "Error: " . $delete_query . "<br>" . $conn->error;
+          }
+        }
+        else {
+          echo "<p style='color:red;'>There must be at least 2 colors in database in order to delete a color.</p>";
+        }
+      }
     ?>
+
+    <h2>Add/Edit/Delete Colors</h2>
+
+    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+      <h3>Add a Color:</h3>
+      Color Name: <input type="text" name="color_name" required><br>
+      Hex Value: <input type="text" name="hex_value" pattern="#[0-9a-fA-F]{6}" title="Enter a valid hex color value (e.g., #RRGGBB)" required><br>
+      <input type="submit" name="add_color" value="Add Color">
+    </form>
+
+    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+      <h3>Delete a Color:</h3>
+      <select name='colors_list'>
+        <?php 
+          $colors_query = "SELECT id, Name FROM colors";
+          $colors_result = $conn->query($colors_query);
+          while ($color_row = $colors_result->fetch_assoc()) {
+            echo "<option value='{$color_row['id']}'>{$color_row['Name']}</option>";
+          }
+        ?>
+      </select>
+      <input type='submit' name='delete_color' value='Delete Color'>
+    </form>
 
     <?php
     $conn->close();
     ?>
-
   </main>
 </body>
 <footer>
