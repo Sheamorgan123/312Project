0c43300819ef37cfeed34e54c70c9655e9cf77a2
mika-ela011 <michelle.r.cortes@hotmail.com>
2024-04-30 15:22:18 -0600

    add implementation

--- MasterCoders/color-selection.php
+++ MasterCoders/color-selection.php
@@ -10,14 +10,65 @@
     <?php include 'navbar.html' ?>
   </header>
   <main>
+    <?php
+    /*
+    code for the database connection,
+    everyone needs to change this to your own database params.
+    */
+    $servername = "faure";
+    $username = "";
+    $password = "";
+    $dbname = "";
 
-    <!-- On this page there should 3 Interfaces. 
-    The first interface is to Add a new color, 
-    ideally the user should only have to enter a
-    name and hex value for the color. If adding a 
-    color would conflict with an already existing 
-    name or hex value, a non-intrusive error should 
-    be displayed telling the user so. -->
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Could not connect to server. " . $conn->connect_error);
+    }
+    echo "<script>console.log('Successfully connected to server.');</script>";
+    ?>
+
+    <h2>Add/Edit/Delete Colors</h2>
+    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+      <h3>Add a Color:</h3>
+      Color Name: <input type="text" name="color_name" required><br>
+      Hex Value: <input type="text" name="hex_value" pattern="#[0-9a-fA-F]{6}" title="Enter a valid hex color value (e.g., #RRGGBB)" required><br>
+      <input type="submit" name="add_color" value="Add Color">
+      <h3>Delete a Color:</h3>
+      <h3>Edit a Color:</h3>
+    </form>
+
+    <?php
+      if (isset($_POST['add_color'])) {
+        $color_name = $_POST['color_name'];
+        $hex_value = $_POST['hex_value'];
+
+        // Check if color already exists
+        $check_query = $conn->prepare("SELECT * FROM colors WHERE Name=? OR hex_value=?");
+        $check_query->bind_param("ss", $color_name, $hex_value);
+        $check_query->execute();
+        $result = $check_query->get_result();
+      
+        if ($result->num_rows > 0) {
+          echo "<p style='color: red;'>Color already exists in the database.</p>";
+        }
+        else {
+          $insert_query = $conn->prepare("INSERT INTO colors (Name, hex_value) VALUES (?, ?)");
+          $insert_query->bind_param("ss", $color_name, $hex_value);
+          $insert_query->execute();
+          if ($insert_query->get_result()) {
+            echo "<p style='color: green;'>Color added successfully.</p>";
+          }
+          else {
+            echo "Error: " . $insert_query . "<br>" . $conn->error;
+          }
+        }
+      }
+    ?>
+
+    <?php
+    $conn->close();
+    ?>
 
   </main>
 </body>
