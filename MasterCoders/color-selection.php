3d48773133406e23bdddc7c8149b76944da7cb48
mika-ela011 <michelle.r.cortes@hotmail.com>
2024-04-30 13:07:29 -0600

    basic implementation

---
+++ MasterCoders/color-selection.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Master Coders | Color Selection </title>
+  <?php include 'header.html' ?>
+  <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+  <header>
+    <?php include 'navbar.html' ?>
+  </header>
+  <main>
+
+    <!-- On this page there should 3 Interfaces. 
+    The first interface is to Add a new color, 
+    ideally the user should only have to enter a
+    name and hex value for the color. If adding a 
+    color would conflict with an already existing 
+    name or hex value, a non-intrusive error should 
+    be displayed telling the user so. -->
+
+  </main>
+</body>
+<footer>
+  <?php include 'footer.html' ?>
+</footer>
+</html>
